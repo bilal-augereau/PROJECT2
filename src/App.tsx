28187f6bff7dae084192fefb34909079f4fd3b50
BILAL AUGEREAU <bilal.augereau@gmail.com>
2024-11-07 11:05:13 +0100

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home.tsx";
+import Search from "./pages/Search.tsx";
 
 function App() {
 	const [plant, setPlant] = useState(null);
@@ -12,7 +15,7 @@ function App() {
 		const options = {
 			method: "GET",
 			headers: {
-				"x-rapidapi-key": "9db15f58b8msh5101d3f3e72beaep175fadjsn258b615b608e", // Remplace par ta propre clé API
+				"x-rapidapi-key": "9db15f58b8msh5101d3f3e72beaep175fadjsn258b615b608e",
 				"x-rapidapi-host": "house-plants2.p.rapidapi.com",
 			},
 		};
@@ -41,31 +44,49 @@ function App() {
 
 	console.log(plant);
 	return (
-		<div>
-			<h1>Liste des Plantes</h1>
-			<ul>
-				{plant.map((item, index) => (
-					<li key={index}>
-						{/* Check if both 'Temperature max' and 'Temperature min' exist */}
-						{item["Temperature max"]?.C && item["Temperature min"]?.C ? (
-							// Calculate and display the difference if both exist
-							<span>
-								{item["Temperature max"].C - item["Temperature min"].C}°C
-							</span>
-						) : item["Temperature max"]?.C && !item["Temperature min"]?.C ? (
-							// If 'Temperature min' is missing, display 'Temperature min not available'
-							<span>
-								{item["Temperature max"].C}°C (Min temperature data not
-								available)
-							</span>
-						) : (
-							// If neither 'Temperature max' nor 'Temperature min' exist, display 'Temperature data not available'
-							<span>Temperature data not available</span>
-						)}
-					</li>
-				))}
-			</ul>
-		</div>
+		<Router>
+			<div>
+				{/* Navigation */}
+				<nav>
+					<ul>
+						<li>
+							<Link to="/">Accueil</Link>
+						</li>
+						<li>
+							<Link to="/search">Search</Link>
+						</li>
+					</ul>
+				</nav>
+
+				{/* Définir les Routes */}
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/search" element={<Search />} />
+				</Routes>
+				<div>
+					<h1>Liste des Plantes</h1>
+					<ul>
+						{plant.map((item, index) => (
+							<li key={index}>
+								{item["Temperature max"]?.C && item["Temperature min"]?.C ? (
+									<span>
+										{item["Temperature max"].C - item["Temperature min"].C}°C
+									</span>
+								) : item["Temperature max"]?.C &&
+									!item["Temperature min"]?.C ? (
+									<span>
+										{item["Temperature max"].C}°C (Min temperature data not
+										available)
+									</span>
+								) : (
+									<span>Temperature data not available</span>
+								)}
+							</li>
+						))}
+					</ul>
+				</div>
+			</div>
+		</Router>
 	);
 }
 
